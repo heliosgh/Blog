fe97656ab4009ae29243b538306512ae01e8066b
Eduardo Hernandez <lalohernandezcar@gmail.com>
2024-03-11 13:17:43 -0600

    js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-console.log("Hola mundos");
+// Cambia el color de fondo al hacer clic en el botón
+const boton = document.querySelector('#miBoton');
+boton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'lightblue';
+});
