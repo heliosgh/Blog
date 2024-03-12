35038f3fb93f468a2c43fcb629d409b2e3423240
Heliosgh <lalohernandezcar@gmail.com>
2024-03-11 20:39:29 -0600

    confirma

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 // Cambia el color de fondo al hacer clic en el botón
 const boton = document.querySelector('#miBoton');
+let fondoRosa = true;
+
 boton.addEventListener('click', () => {
-    document.body.style.backgroundColor = 'lightblue';
+    const body = document.body;
+    if (fondoRosa) {
+        body.style.backgroundColor = 'blue'; // Cambia a azul
+    } else {
+        body.style.backgroundColor = '#fdcae1'; // Cambia a rosa
+    }
+    fondoRosa = !fondoRosa; // Alterna el estado
 });
